004fc6139f7d4ef6ed878de328f27878a60dfe38
S4lju0n3 <108986123+S4lju0n3@users.noreply.github.com>
2023-01-04 15:20:37 +0700

    Add files via upload

---
+++ kmrh/sugest/siip/changepass.php
@@ -0,0 +1,5 @@
+<?php
+/*
+update password
+{ "username=", Me.TextBox1.Text, "&oldpassword=", Me.TextBox2.Text, "&newpassword=", Me.TextBox3.Text })
+*/
---
+++ kmrh/sugest/siip/tgl.php
@@ -0,0 +1,3 @@
+<?php
+date_default_timezone_set("Asia/Jakarta");
+echo utf8_encode(date('Y-m-d H:i:s'));

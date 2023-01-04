cbb8ffcd5bac2b41ecbc7a0099fd4391f44b0187
S4lju0n3 <108986123+S4lju0n3@users.noreply.github.com>
2023-01-04 15:18:41 +0700

    Delete qctool directory

--- qctool/login/changepass.php
+++
@@ -1,5 +0,0 @@
-<?php
-/*
-update password
-{ "username=", Me.TextBox1.Text, "&oldpassword=", Me.TextBox2.Text, "&newpassword=", Me.TextBox3.Text })
-*/
--- qctool/login/tanggal.php
+++
@@ -1,3 +0,0 @@
-<?php
-date_default_timezone_set("Asia/Jakarta");
-echo utf8_encode(date('Y-m-d H:i:s'));
